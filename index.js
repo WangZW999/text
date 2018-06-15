925595c0a5220734573e6c2deea9269f5529d36c
WangZW999 <13522320134@163.com>
2018-06-15 14:31:42 +0800

    完整选项卡

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 
-var oP=document.getElementById("options");
-var oL=document.getElementById("list");
-var oLi=oP.getElementsByTagName("li");
-var oBox=oL.getElementsByTagName("li");
+var oP = document.getElementById("options");
+var oL = document.getElementById("list");
+var oLi = oP.getElementsByTagName("li");
+var oBox = oL.getElementsByTagName('li');
 for(var i=0;i<oLi.length;i++){
-    oLi[i].index=i;
+    oLi[i].index = i;
     oLi[i].onclick=function(){
-        for(var j=0;j<oLi.length;i++){
-                oLi[i].className="";
-             }
-            oLi[this.index].className="active";
-            for(var i=0;i<oBox.length;i++){
-            	oBox[i].style.display="none";
-            }
-            oBox[this.index].style.display="block";
+        for(var j=0;j<oLi.length;j++){
+        	oLi[j].className="";
+        	oBox[j].style.display="none";
+        }
+        oLi[this.index].className="active";
+        oBox[this.index].style.display="block";
     }
 }
